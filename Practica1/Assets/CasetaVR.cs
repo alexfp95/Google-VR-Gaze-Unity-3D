caf9ca511b7b984e984fcb021569245edcbe65d7
Alexis Daniel Fuentes Pérez <alex.fp95@icloud.com>
2016-10-21 10:10:37 +0100

    Uploading project

---
+++ Practica1/Assets/CasetaVR.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CasetaVR : MonoBehaviour, IGvrGazeResponder {
+
+	public void OnGazeEnter () {
+		Debug.Log ("Gaze: Enter");
+		transform.Rotate (0, 0, 45);
+	}
+
+	public void OnGazeExit () { 
+		Debug.Log ("Gaze: Exit");
+		transform.Rotate (0, 0, 45);
+	}
+
+	public void OnGazeTrigger () {
+		Debug.Log ("Gaze: Trigger");
+		transform.Rotate (0, 0, 45);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Practica1/Assets/personScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class personScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnCollisionEnter (Collision collision) {
+		Debug.Log ("Choco con: " + collision.gameObject.name);
+	}
+}
